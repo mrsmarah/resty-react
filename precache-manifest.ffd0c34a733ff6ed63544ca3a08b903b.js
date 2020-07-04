6354864f5857bfd9843ce3f676c28d1942ade7ad
mrsmarah <runner@fv-az70.jeyvzqmxcxxedhxe5u50yechyf.bx.internal.cloudapp.net>
2020-07-04 15:35:18 +0000

    Updates

---
+++ precache-manifest.ffd0c34a733ff6ed63544ca3a08b903b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf5945f737196350638e39d3b2968c3",
+    "url": "/resty-react/index.html"
+  },
+  {
+    "revision": "92590b5b6631a5cdf18c",
+    "url": "/resty-react/static/css/main.824843ae.chunk.css"
+  },
+  {
+    "revision": "658d6a4fca0f58040033",
+    "url": "/resty-react/static/js/2.fbc0beee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resty-react/static/js/2.fbc0beee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92590b5b6631a5cdf18c",
+    "url": "/resty-react/static/js/main.e2c3dd5a.chunk.js"
+  },
+  {
+    "revision": "394448507ae43605b08a",
+    "url": "/resty-react/static/js/runtime-main.3d11c8ef.js"
+  }
+]);
